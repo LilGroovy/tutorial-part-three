010580d5087ca0e0b085d00822d5550e43a64653
Nikolai Altergott <nikolai.altergott.na@gmail.com>
2021-04-11 14:23:24 +0200

    test

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout"
 export default function About() {
   return (
     <Layout>
-      <h1>About me</h1>
+      <h1>About Nikolai The Great</h1>
       <p>
         I’m good enough, I’m smart enough, and gosh darn it, people like me!
       </p>
